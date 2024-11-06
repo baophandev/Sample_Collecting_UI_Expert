95eaba00161c794d537b9811216e160a0d7eafcc
TLNYNYNY <91382054+TLNYNYNY@users.noreply.github.com>
2024-11-06 14:40:49 +0700

    Update: getRefreshKey in ProjectPagingSource

--- app/src/main/java/com/application/data/paging/ProjectPagingSource.kt
+++ app/src/main/java/com/application/data/paging/ProjectPagingSource.kt
@@ -20,14 +20,14 @@ class ProjectPagingSource(
     private val attachmentRepository: AttachmentRepository
 ) : PagingSource<Int, Project>() {
 
-    override fun getRefreshKey(state: PagingState<Int, Project>): Int {
-        // Try to find the page key of the closest page to anchorPosition from
-        // either the prevKey or the nextKey; you need to handle nullability here.
-        //  * prevKey == null -> anchorPage is the first page.
-        //  * nextKey == null -> anchorPage is the last page.
-        //  * both prevKey and nextKey are null -> anchorPage is the initial page, so return null.
-        return ((state.anchorPosition ?: 0) - state.config.initialLoadSize / 2)
-            .coerceAtLeast(0)
+    override fun getRefreshKey(state: PagingState<Int, Project>): Int? {
+        // We need to get the previous key (or next key if previous is null) of the page
+        // that was closest to the most recently accessed index.
+        // Anchor position is the most recently accessed index
+        return state.anchorPosition?.let { anchorPosition ->
+            state.closestPageToPosition(anchorPosition)?.prevKey?.plus(1)
+                ?: state.closestPageToPosition(anchorPosition)?.nextKey?.minus(1)
+        }
     }
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Project> {
@@ -42,10 +42,11 @@ class ProjectPagingSource(
             )
 
             val projects = response.content.map { mapResponseToProject(it) }
+            val nextKey = if (!response.last) nextPageNumber + 1 else null
             return LoadResult.Page(
                 data = projects,
                 prevKey = null, // Only paging forward.
-                nextKey = if (nextPageNumber + 1 < response.totalPages) nextPageNumber + 1 else null
+                nextKey = nextKey
             )
         } catch (e: IOException) {
             Log.e(TAG, e.message, e)
